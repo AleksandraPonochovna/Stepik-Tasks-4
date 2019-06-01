4d755ccb15ee2ed8f1753ff2a2c9b2e788886471
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-01 12:23:24 +0300

    Update HW3.4.java

--- HW3.4.java
+++ HW3.4.java
@@ -1,36 +1,40 @@
 package com.company;
 
-public final class ComplexNumberApp {
+public final class ComplexNumber {
 
     private final double re;
     private final double im;
 
-    public static void main(String[] args) {
-        ComplexNumberApp firstNumber = new ComplexNumberApp(1, 1);
-        ComplexNumberApp secondNumber = new ComplexNumberApp(1, 1);
-        System.out.print("firstNumber.equals(secondNumber) is ");
-        System.out.println(firstNumber.equals(secondNumber));
-        System.out.print("firstNumber.hashCode() ==  secondNumber.hashCode() is ");
-        System.out.println(firstNumber.hashCode(firstNumber.getRe(), firstNumber.getIm()) == secondNumber.hashCode(secondNumber.getRe(),secondNumber.getIm()));
-    }
-
-    public boolean equals(ComplexNumber complexNumber) {
-        return (this.getIm() == complexNumber.getIm()) && (this.getRe() == complexNumber.getRe());
-    }
-
-    public double hashCode(double re, double im) {
-        double  result = 17;
-        long longBitsRe = Double.doubleToLongBits(re);
-        long longBitsIm = Double.doubleToLongBits(im);
-        double resultRe = 37 * result + (int)(longBitsRe - (longBitsRe >>> 32));
-        double resultIm = 37 * result + (int)(longBitsIm - (longBitsIm >>> 32));
-        result = resultIm + resultRe;
-        return result;
-    }
-
     public ComplexNumber(double re, double im) {
         this.re = re;
         this.im = im;
+    }
+
+    public static void main(String[] args) {
+        ComplexNumber firstNumber = new ComplexNumber(22, 22);
+        ComplexNumber secondNumber = new ComplexNumber(22, 22);
+        System.out.print("firstNumber.equals(secondNumber) is ");
+        System.out.println(firstNumber.equals(secondNumber));
+        System.out.print("firstNumber.hashCode() ==  secondNumber.hashCode() is ");
+        System.out.println(firstNumber.hashCode() == secondNumber.hashCode());
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        ComplexNumber firstNumber = (ComplexNumber) obj;
+        return (getRe() == firstNumber.getRe() && getIm() == firstNumber.getIm());
+    }
+
+    public int hashCode() {
+        int result = 1;
+        final int constant = 31;
+        result = (int) (constant * result + getIm() * constant + getRe() * constant);
+        return result;
     }
 
     public double getRe() {
@@ -41,3 +45,4 @@ public final class ComplexNumberApp {
         return im;
     }
 }
+
