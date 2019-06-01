a04a6704ed2662b264c29b2555939b031e235d16
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-01 12:03:40 +0300

    Update HW3.3.java

--- HW3.3.java
+++ HW3.3.java
@@ -1,87 +1,27 @@
 package com.company;
 
-public class RobotApp {
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+package com.company;
+
+public class Robot {
+    Direction direction = Direction.UP;
     private int x = 0;
     private int y = 0;
-    Direction direction = Direction.DOWN;
-
-    public enum Direction {
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void turnLeft() {
-        System.out.println("Поворот на 90 градусов против часовой стрелки.");
-        switch (getDirection()) {
-            case UP:
-                direction = direction.LEFT;
-                break;
-            case DOWN:
-                direction = direction.RIGHT;
-                break;
-            case LEFT:
-                direction = direction.UP;
-                break;
-            case RIGHT:
-                direction = direction.DOWN;
-                break;
-        }
-    }
-
-    public void turnRight() {
-        System.out.println("Поворот на 90 градусов по часовой стрелке.");
-        switch (getDirection()){
-            default:
-            case DOWN:
-                direction = Direction.LEFT;
-                break;
-            case UP:
-                direction = Direction.RIGHT;
-                break;
-            case RIGHT:
-                direction = Direction.DOWN;
-                break;
-            case LEFT:
-                direction = Direction.UP;
-        }
-    }
-
-    public void stepForward() {
-        switch (getDirection()){
-            default:
-            case RIGHT:
-                x++;
-                System.out.println("Шаг направо.");
-                break;
-            case LEFT:
-                x--;
-                System.out.println("Шаг налево.");
-                break;
-            case UP:
-                y++;
-                System.out.println("Шаг вперед.");
-                break;
-            case DOWN:
-                y--;
-                System.out.println("Шаг назад.");
-                break;
-        }
-   }
 
     public static void main(String[] args) {
         int toX = 3;
         int toY = 0;
-        RobotApp robot = new RobotApp();
+        Robot robot = new Robot();
         robot.moveRobot(robot, toX, toY);
     }
 
-    public static void moveRobot(RobotApp robot, int toX, int toY) {
+    public static void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getY() < toY) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
@@ -114,6 +54,69 @@ public class RobotApp {
         }
     }
 
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void turnLeft() {
+        System.out.println("Rotate 90 degrees counterclockwise.");
+        switch (getDirection()) {
+            case UP:
+                direction = direction.LEFT;
+                break;
+            case DOWN:
+                direction = direction.RIGHT;
+                break;
+            case LEFT:
+                direction = direction.UP;
+                break;
+            case RIGHT:
+                direction = direction.DOWN;
+                break;
+        }
+    }
+
+    public void turnRight() {
+        System.out.println("Rotate 90 degrees clockwise.");
+        switch (getDirection()) {
+            default:
+            case DOWN:
+                direction = Direction.LEFT;
+                break;
+            case UP:
+                direction = Direction.RIGHT;
+                break;
+            case RIGHT:
+                direction = Direction.DOWN;
+                break;
+            case LEFT:
+                direction = Direction.UP;
+        }
+
+    }
+
+    public void stepForward() {
+        switch (getDirection()) {
+            default:
+            case RIGHT:
+                x++;
+                System.out.println("Шаг направо.");
+                break;
+            case LEFT:
+                x--;
+                System.out.println("Шаг налево.");
+                break;
+            case UP:
+                y++;
+                System.out.println("Шаг вперед.");
+                break;
+            case DOWN:
+                y--;
+                System.out.println("Шаг назад.");
+                break;
+        }
+    }
+
     public int getX() {
         return x;
     }
@@ -121,4 +124,5 @@ public class RobotApp {
     public int getY() {
         return y;
     }
+
 }
